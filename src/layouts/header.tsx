c3ecdc12744199456a5a1bd5d54f114349a52e54
Calum Harris <harris.calum@gmail.com>
2020-05-24 18:29:21 +0100

    header

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, TouchableOpacity } from "react-native"
-import { Headline } from "react-native-paper"
+import { Headline, Text, Title } from "react-native-paper"
 import Icon from "react-native-vector-icons/MaterialCommunityIcons"
+import { navigate } from "gatsby"
 
 const Header = () => {
+
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const toggleOpen = () => {
+        setOpenMenu(!openMenu)
+    }
+
     return <View style={{flexDirection:"row", alignItems:"center", padding:16}}>
-        <TouchableOpacity style={{ marginRight: 8 }}>
-            <Icon name="menu" size={30} style={{justifyContent:"center"}}/>
+        <TouchableOpacity style={{ marginRight: 8 }} onPress={toggleOpen}>
+            <Icon name={openMenu ? "close" : "menu"} size={30} style={{justifyContent:"center"}}/>
         </TouchableOpacity>
-        <Headline>LIANA MORAN</Headline>
+        <TouchableOpacity onPress={() => navigate("/")}>
+            <Headline style={{fontStyle: "italic"}} >LIANA MORAN</Headline>
+        </TouchableOpacity>
     </View>
 }
 
